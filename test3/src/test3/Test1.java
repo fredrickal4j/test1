d3405e2f58302a84ae8c816ecf0415747adc3368
tarena <tarena@tarena-pc>
2019-05-30 16:01:05 +0800

    v1.7

--- test3/src/test3/Test1.java
+++ test3/src/test3/Test1.java
@@ -9,7 +9,7 @@ public static void main(String[] args) {
 
 	System.out.println("branch22222nch");
 	System.out.println("master召唤");
-	System.out.println("bitchasda");
+	System.out.println("bitchasdfsfa");
 
 
 }
