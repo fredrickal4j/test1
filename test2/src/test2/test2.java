c0234302acfdb5da346bc59873c2e4ace67b8826
tarena <tarena@tarena-pc>
2019-05-30 14:41:39 +0800

    v1.3

--- test2/src/test2/test2.java
+++ test2/src/test2/test2.java
@@ -3,5 +3,6 @@ package test2;
 public class test2 {
 public static void main(String[] args) {
 	System.out.println("hello2");
+	System.out.println("change");
 }
 }
---
+++ test2/src/test2/test4.java
@@ -0,0 +1,7 @@
+package test2;
+
+public class test4 {
+public static void main(String[] args) {
+	System.out.println("4j check");
+}
+}
